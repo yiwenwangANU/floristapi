c8dea5ace386190422dd2ff4eaf0b055bc9bc6ca
Yiwen Wang <wangyiwenau@gmail.com>
2025-08-05 09:32:22 +1000

    move validation to dto

--- FloristApi/Models/Entities/Flower.cs
+++ FloristApi/Models/Entities/Flower.cs
@@ -1,6 +1,17 @@
-﻿namespace FloristApi.Models.Entities
+﻿using FloristApi.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace FloristApi.Models.Entities
 {
     public class Flower
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public int Price { get; set; };
+        public int Discount { get; set; } = 0;
+        public ProductTypes ProductType { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
