85cadf34a7db3444fa03b26a20d384a6595593fc
Yiwen Wang <wangyiwenau@gmail.com>
2025-08-06 16:03:57 +1000

    create controller for get/create flowers

--- FloristApi/Controllers/FlowerController.cs
+++ FloristApi/Controllers/FlowerController.cs
@@ -1,6 +1,30 @@
-﻿namespace FloristApi.Controllers
+﻿using FloristApi.Models.Dtos;
+using FloristApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+
+namespace FloristApi.Controllers
 {
-    public class FlowerController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlowerController : ControllerBase
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IFlowerService _flowerService;
+        public FlowerController(UserManager<IdentityUser> userManager, IFlowerService flowerService)
+        {
+            _userManager = userManager;
+            _flowerService = flowerService;
+        }
+        [HttpPost("createFlower")]
+        public async Task<IActionResult> createFlower(CreateFlowerDto dto)
+        {
+            var response = await _flowerService.CreateFlower(dto);
+            return Ok(response);
+        }
     }
+
 }
