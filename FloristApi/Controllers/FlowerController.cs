51d39fbd70a631f0be645bc1b2ef882c457cacb1
Yiwen Wang <wangyiwenau@gmail.com>
2025-08-06 10:21:20 +1000

    create flowerservice and its interface

---
+++ FloristApi/Controllers/FlowerController.cs
@@ -0,0 +1,6 @@
+﻿namespace FloristApi.Controllers
+{
+    public class FlowerController
+    {
+    }
+}
---
+++ FloristApi/Services/FlowerService.cs
@@ -0,0 +1,25 @@
+﻿using FloristApi.Models.Dtos;
+
+namespace FloristApi.Services
+{
+    public class FlowerService: IFlowerService
+    {
+        
+        public Task<GetFlowerResponse> CreateFlower(CreateFlowerDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<GetFlowerResponse>> IFlowerService.GetFlowers()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<GetFlowerResponse> IFlowerService.GetFlowerById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    {
+    }
+}
