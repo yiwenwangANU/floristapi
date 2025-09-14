c4f62b2ea0e1ce3c235f6c754e51bdb388032259
Yiwen Wang <wangyiwenau@gmail.com>
2025-09-14 11:19:57 +1000

    create getflowerDto

---
+++ FloristApi/Models/Dtos/public/GetFlowerDto.cs
@@ -0,0 +1,23 @@
+﻿using FloristApi.Enums;
+
+namespace FloristApi.Models.Dtos.@public
+{
+    public class GetFlowerDto
+    {
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 12;
+        public ProductTypes? ProductType { get; set; }
+        public ColorTypes? Color { get; set; }
+        public OccasionTypes? Occasion { get; set; }
+        public FlowerTypes? FlowerType { get; set; }
+        public int? MinPrice { get; set; }
+        public int? MaxPrice { get; set; }
+        public string? SearchTerm { get; set; } = string.Empty;
+        public SortBy Sort { get; set; } = SortBy.IdAsc;
+    }
+
+    public enum SortBy
+    {
+        IdAsc, IdDesc, PriceAsc, PriceDesc
+    }
+}
