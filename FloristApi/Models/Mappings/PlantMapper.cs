ef2816e517eb00a2b3133d14125acc86ea57fe1d
Yiwen Wang <wangyiwenau@gmail.com>
2025-09-21 11:54:05 +1000

    create plantmapper

---
+++ FloristApi/Models/Mappings/PlantMapper.cs
@@ -0,0 +1,38 @@
+﻿using FloristApi.Models.Dtos.admin;
+using FloristApi.Models.Dtos.@public;
+using FloristApi.Models.Entities;
+
+namespace FloristApi.Models.Mappings
+{
+    public static class PlantMapper
+    {
+        public static Plant ToEntity(this CreatePlantDto dto)
+        {
+            return new Plant
+            {
+                Name = dto.Name,
+                Description = dto.Description,
+                ImageUrl = dto.ImageUrl,
+
+                PlantType = dto.PlantType,
+
+                Price = dto.Price,
+                Discount = dto.Discount ?? 0,
+            };
+        }
+
+        public static GetPlantResponse ToResponse(this Plant plant)
+        {
+            return new GetPlantResponse
+            {
+                Id = plant.Id,
+                Name = plant.Name,
+                Description = plant.Description,
+                ImageUrl = plant.ImageUrl,
+                Price = plant.Price,
+                PlantType = plant.PlantType,
+                Discount = plant.Discount
+            };
+        }
+    }
+}
